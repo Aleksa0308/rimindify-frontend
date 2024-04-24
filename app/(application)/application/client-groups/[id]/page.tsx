33dcdc384bcde76524479845467ce93aa0026a76
Aleksa0308 <aleksas0308@gmail.com>
2024-04-24 16:21:32 +0200

    feat: client groups single add and display

--- app/(application)/application/client-groups/[id]/page.tsx
+++ app/(application)/application/client-groups/[id]/page.tsx
@@ -7,6 +7,7 @@ import { Trash2 } from 'lucide-react'
 import { ActionConfirmationDialog } from '@/app/(application)/application/action-confirmation-dialog'
 import * as React from 'react'
 import { useState } from 'react'
+import AddClientForm from '@/app/(application)/application/client-groups/[id]/add-client-form'
 
 export default function ClientGroupById() {
     const router = useParams<{ id: string }>()
@@ -15,7 +16,7 @@ export default function ClientGroupById() {
     const [dialogOpen, setDialogOpen] = useState(false)
     return (
         <section className="container mt-4 flex flex-col gap-2">
-            <div className="flex align-middle justify-between">
+            <div className="flex items-center justify-between">
                 {/* Display info */}
                 <div>
                     <h1 className="text-lg font-semibold">
@@ -29,25 +30,27 @@ export default function ClientGroupById() {
                             : 'Loading...'}
                     </blockquote>
                 </div>
-                <div>Add Client</div>
-                {/*    Delete Button*/}
-                <ActionConfirmationDialog
-                    buttonText="Delete"
-                    open={dialogOpen}
-                    dialogOnChange={setDialogOpen}
-                    dialogTitle="Delete Group"
-                    dialogDescription="Are you sure you want to delete this group?"
-                    onAction={(confirmed) => {
-                        if (confirmed) {
-                            // delete message
-                            console.log('delete group')
-                        }
-                    }}
-                    buttonVariant={'destructive'}
-                    buttonIcon={<Trash2 className="h-4 w-4" />}
-                    buttonStyle={'flex gap-2 self-end w-fit'}
-                />
-                {/* Quick Add Client to group*/}
+                {/*Add Clients*/}
+                <AddClientForm clientGroupId={clientGroupId} />
+                <div className="flex gap-2 items-center">
+                    {/*    Delete Button*/}
+                    <ActionConfirmationDialog
+                        buttonText="Delete"
+                        open={dialogOpen}
+                        dialogOnChange={setDialogOpen}
+                        dialogTitle="Delete Group"
+                        dialogDescription="Are you sure you want to delete this group?"
+                        onAction={(confirmed) => {
+                            if (confirmed) {
+                                // delete message
+                                console.log('delete group')
+                            }
+                        }}
+                        buttonVariant={'destructive'}
+                        buttonIcon={<Trash2 className="h-4 w-4" />}
+                        buttonStyle={'flex gap-2 w-fit'}
+                    />
+                </div>
             </div>
             {/* Display clients in table */}
             <DataTable
